606f1e876dca6c50a9043b9fb22b76915135ec2f
Costava <Costava@users.noreply.github.com>
2020-09-03 21:11:00 -0500

    Revise main.c

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
-// https://github.com/Costava/sdl2-draw-demo
-
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -16,41 +15,59 @@
 int screen_width = 512;
 int screen_height = 512;
 
-int clamp(int val, int min, int max) {
+int clamp(const int val, const int min, const int max) {
 	if (val < min) return min;
 	if (val > max) return max;
 
 	return val;
 }
 
-void set_pixel(SDL_Surface *surface, int x, int y, Uint8 r, Uint8 g, Uint8 b) {
-	Uint32 *pixels = (Uint32 *) surface->pixels;
+// Return true or false with ~equal probability
+bool rand_bool(void) {
+	if (rand() > RAND_MAX / 2) return true;
 
-	Uint32 color = SDL_MapRGB(surface->format, r, g, b);
+	return false;
+}
+
+void set_pixel(
+	SDL_Surface *const surface,
+	const int x,
+	const int y,
+	const Uint8 r,
+	const Uint8 g,
+	const Uint8 b)
+{
+	Uint32 *const pixels = (Uint32 *) surface->pixels;
+
+	const Uint32 color = SDL_MapRGB(surface->format, r, g, b);
 
 	pixels[x + (y * surface->w)] = color;
 }
 
-int main(int argc, char *argv[]) {
-	SDL_Window *window = NULL;
-	SDL_Surface *surface = NULL;
-
-	// Initialize SDL
-	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+int main(void) {
+	// Initialize SDL2
+	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
 		printf("SDL_Init failed: %s\n", SDL_GetError());
-
 		exit(1);
 	}
 
-	window = SDL_CreateWindow("Draw", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, screen_width, screen_height, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+	SDL_Window *const window = SDL_CreateWindow(
+		"Draw",
+		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+		screen_width, screen_height,
+		SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
 
 	if (window == NULL) {
 		printf("SDL_CreateWindow failed: %s\n", SDL_GetError());
-
 		exit(1);
 	}
 
-	surface = SDL_GetWindowSurface(window);
+	SDL_Surface *surface = SDL_GetWindowSurface(window);
+
+	if (surface == NULL) {
+		printf("SDL_GetWindowSurface failed: %s\n", SDL_GetError());
+		exit(1);
+	}
 
 	// Initialize window to all black
 	SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0, 0, 0));
@@ -68,29 +85,34 @@ int main(int argc, char *argv[]) {
 	Uint8 g = 128;
 	Uint8 b = 128;
 
-	SDL_Event event;
-	int quit = 0;
-	int drawing = 0;
+	bool quit = false;
+	bool drawing = false;
 
 	while (!quit) {
+		SDL_Event event;
 		while (SDL_PollEvent(&event)) {
 			if (event.type == SDL_QUIT) {
-				quit = 1;
+				quit = true;
 			}
 			else if (event.type == SDL_WINDOWEVENT) {
 				if (event.window.event == SDL_WINDOWEVENT_SIZE_CHANGED) {
 					surface = SDL_GetWindowSurface(window);
 
-					SDL_GetWindowSize(window, &screen_width, &screen_height);
+					if (surface == NULL) {
+						printf("SDL_GetWindowSurface failed: %s\n",
+							SDL_GetError());
+						exit(1);
+					}
 
+					SDL_GetWindowSize(window, &screen_width, &screen_height);
 					SDL_UpdateWindowSurface(window);
 				}
 			}
 			else if (event.type == SDL_MOUSEBUTTONDOWN) {
-				drawing = 1;
+				drawing = true;
 			}
 			else if (event.type == SDL_MOUSEBUTTONUP) {
-				drawing = 0;
+				drawing = false;
 			}
 			else if (event.type == SDL_MOUSEMOTION) {
 				mouse_x = event.motion.x;
@@ -102,19 +124,19 @@ int main(int argc, char *argv[]) {
 		}
 
 		if (drawing) {
-			if (rand() > RAND_MAX / 2) x += 1;
+			if (rand_bool()) x += 1;
 			else x -= 1;
 
-			if (rand() > RAND_MAX / 2) y += 1;
+			if (rand_bool()) y += 1;
 			else y -= 1;
 
-			if (rand() > RAND_MAX / 2) r += 1;
+			if (rand_bool()) r += 1;
 			else r -= 1;
 
-			if (rand() > RAND_MAX / 2) g += 1;
+			if (rand_bool()) g += 1;
 			else g -= 1;
 
-			if (rand() > RAND_MAX / 2) b += 1;
+			if (rand_bool()) b += 1;
 			else b -= 1;
 
 			x = clamp(x, 0, screen_width - 1);
